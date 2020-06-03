e9ed796a12bbcdb09677e6e086220411e0043e90
Eli Cusic <tntitans31@gmail.com>
2020-06-03 13:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function whisper(string){
   return string.toLowerCase()
 }
     //1) receives one argument and returns it in all lowercase
- 
+
 function logShout(string){
   console.log(string.toUpperCase())
 }
     //2) calls console.log() its one argument in all caps
- 
+
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
     //3) calls console.log() its one argument in all lowercase
- 
+
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
